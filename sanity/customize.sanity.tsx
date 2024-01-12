c13919c8b7ccd4ad4c523fc10e7dc4367efc7476
suman chapai <sumanchapai@gmail.com>
2024-01-12 13:14:20 +0545

    Enhance simpler sanity desk structure api

--- sanity/customize.sanity.tsx
+++ sanity/customize.sanity.tsx
@@ -1,28 +1,19 @@
-// Theme customization, desk structure here 
+// Theme customization, desk structure here
 // Add plugins in the sanity.config.ts(x) file
-import { CustomDeskGroupType } from "./api.desk-structure.ts"
+import type { CustomDeskGroupType } from '@/sanity/api.desk-structure.ts'
+import { article, settings } from '@/sanity/schemas/documents'
 
-// Edit for desired title (appears in the top left of the studio)  
+// Edit for desired title (appears in the top left of the studio)
 export const studioTitle = 'Studio'
 
 // Customization for the desk structure
 export const customDeskStructure: CustomDeskGroupType = {
 	title: 'Schemas',
+	type: "group",
 	items: [
-
 		// Uncomment the  objects below and replace with the
 		// desk structure you desire.
-
-		// {
-		// 	title: 'Site Settings', items: []
-		// },
-
-		// {
-		// 	icon: () => <div>Settings </div>,
-		// 	title: 'Article',
-		// 	type: 'article',
-		// 	isSingleton: true,
-		// }
-
+		{ type: "singleton", doc: settings },
+		{ type: "doc", doc: article },
 	],
 }
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,24 +1,23 @@
-/**
- * This config is used to set up Sanity Studio that's mounted on the `/pages/studio/[[...index]].tsx` route
- */
 import { visionTool } from '@sanity/vision'
-
-import { clientEnv } from '@/lib/env/client'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
+
+import { clientEnv } from '@/lib/env/client'
+
+import { structure } from './api.desk-structure.ts'
 import { studioTitle } from './customize.sanity'
-//
+import { article, settings } from './schemas/documents'
 
 export default defineConfig({
-	basePath: '/studio',
-	projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
-	dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
-	title: studioTitle,
-	schema: {
-		types: []
-	},
-	plugins: [
-		structureTool(),
-		visionTool({ defaultApiVersion: clientEnv.NEXT_PUBLIC_SANITY_API_VERSION }), // Add GROQ query playground
-	]
+  basePath: '/studio',
+  projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
+  title: studioTitle,
+  schema: {
+    types: [article, settings],
+  },
+  plugins: [
+    structureTool({ structure: structure }),
+    visionTool({ defaultApiVersion: clientEnv.NEXT_PUBLIC_SANITY_API_VERSION }), // Add GROQ query playground
+  ],
 })
