f3c265ba31d91104d38a584083b5b0128bb624ba
suman chapai <sumanchapai@gmail.com>
2024-01-12 14:22:28 +0545

    Fix desk structure schema names

--- sanity/api.desk-structure.ts.ts
+++ sanity/api.desk-structure.ts.ts
@@ -1,11 +1,17 @@
 import type { DocumentDefinition } from 'sanity'
-import type { Divider, ListItem, ListItemBuilder, StructureBuilder, StructureResolverContext } from 'sanity/structure'
+import type {
+  Divider,
+  ListItem,
+  ListItemBuilder,
+  StructureBuilder,
+  StructureResolverContext,
+} from 'sanity/structure'
 
 import { customDeskStructure } from './customize.sanity'
 
 // Public facing API
 export interface CustomDeskGroupType {
-  type: "group"
+  type: 'group'
   icon?: React.ElementType
   title: string
   items: Array<
@@ -14,36 +20,46 @@ export interface CustomDeskGroupType {
 }
 
 export interface CustomDeskDocType {
-  type: "doc"
+  type: 'doc'
   doc: DocumentDefinition
 }
 
 export interface CustomDeskDocSingletonType {
-  type: "singleton"
+  type: 'singleton'
   doc: DocumentDefinition
 }
 
-
 type DeskItem = ListItemBuilder | ListItem | Divider
 
-function insertDividersInMiddle(items: Array<DeskItem>, S: StructureBuilder): Array<DeskItem> {
-  return items.reduce((prev: DeskItem[], curr: DeskItem) => [...prev, S.divider(), curr], [])
+function insertDividersInMiddle(
+  items: Array<DeskItem>,
+  S: StructureBuilder,
+): Array<DeskItem> {
+  return items.reduce(
+    (prev: DeskItem[], curr: DeskItem) => [...prev, S.divider(), curr],
+    [],
+  )
 }
 
 // This function returns builds appropriate structure for a given object
 function getStructure(
   x: CustomDeskDocType | CustomDeskGroupType | CustomDeskDocSingletonType,
   S: StructureBuilder,
-  context: StructureResolverContext
+  context: StructureResolverContext,
 ): DeskItem {
   switch (x.type) {
     // Document type
-    case 'doc':
+    case 'doc': // Note schemaType is not x.doc.type
+    // x.doc.type is always document for 'document' schemas!
+    {
+      const schemaType = x.doc.name
+      const documentTitle = x.doc.title || schemaType
       return S.listItem()
-        .title(x.doc.title || x.doc.type)
+        .title(documentTitle)
         .icon(x.doc.icon)
-        .schemaType(x.doc.type)
-        .child(S.documentTypeList(x.doc.type).title(`All ${x.doc.title || x.doc.type}`))
+        .schemaType(schemaType)
+        .child(S.documentTypeList(schemaType).title(`All ${documentTitle}`))
+    }
     // Group type
     case 'group':
       return S.listItem()
@@ -53,30 +69,37 @@ function getStructure(
           S.list()
             .title(x.title)
             .items(
-              insertDividersInMiddle(x.items.map(y =>
-                getStructure(y, S, context)), S)
-            )
+              insertDividersInMiddle(
+                x.items.map((y) => getStructure(y, S, context)),
+                S,
+              ),
+            ),
         )
     // Singleton
-    case 'singleton':
+    case 'singleton': // Note schemaType is not x.doc.type
+    // x.doc.type is always document for 'document' schemas!
+    {
+      const schemaType = x.doc.name
+      const documentTitle = x.doc.title || schemaType
       return S.listItem()
-        .title(x.doc.title ?? x.doc.name)
+        .title(documentTitle)
         .icon(x.doc.icon)
-        .child(
-          S.document()
-            .schemaType(x.doc.name)
-            .documentId(x.doc.name)
-        )
+        .child(S.document().schemaType(schemaType).documentId(documentTitle))
+    }
     default:
       throw x
   }
 }
 
-
 // Builds desk structure based on custom desk structure
 // ./customize.sanity.tsx
-export const structure = (S: StructureBuilder, context: StructureResolverContext) => {
+export const structure = (
+  S: StructureBuilder,
+  context: StructureResolverContext,
+) => {
   const title = customDeskStructure.title
-  const items = customDeskStructure.items.map(x => getStructure(x, S, context))
+  const items = customDeskStructure.items.map((x) =>
+    getStructure(x, S, context),
+  )
   return S.list().title(title).items(insertDividersInMiddle(items, S))
 }
