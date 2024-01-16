690fb431f20fe9f9d55dfa8cc4a3ff80221a4f32
suman chapai <sumanchapai@gmail.com>
2024-01-16 17:27:45 +0545

    Add server env

---
+++ src/lib/env/server.ts
@@ -0,0 +1,20 @@
+import { formatErrors } from './client'
+import { serverEnvSchema } from './schema'
+
+const serverEnvRaw = {
+  SANITY_API_READ_TOKEN: process.env.SANITY_API_READ_TOKEN
+}
+
+// Parse the env variables provided against the defined schema
+const _serverEnv = serverEnvSchema.safeParse(serverEnvRaw)
+
+// Format error
+if (!_serverEnv.success) {
+  console.error(
+    '❌ Invalid environment variables:\n',
+    ...formatErrors(_serverEnv.error.format()),
+  )
+  throw new Error(`Invalid environment variables`)
+}
+
+export const serverEnv = _serverEnv.data
