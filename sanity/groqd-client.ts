064126e17cd46c3c8501e281cb726e815a68d980
suman chapai <sumanchapai@gmail.com>
2024-01-21 19:14:33 +0545

    Remove flash of published content on preview

--- sanity/groqd-client.ts
+++ sanity/groqd-client.ts
@@ -1,7 +1,13 @@
 import { makeSafeQueryRunner } from 'groqd'
 
+import { serverEnv } from '@/lib/env/server'
+
 import { getClient } from './client'
 
-export const runQuery = makeSafeQueryRunner((query, params, token) =>
-  getClient({ token }).fetch(query, params),
+export const runDraftQuery = makeSafeQueryRunner((query, params) =>
+  getClient({ token: serverEnv.SANITY_API_READ_TOKEN }).fetch(query, params),
+)
+
+export const runQuery = makeSafeQueryRunner((query, params) =>
+  getClient().fetch(query, params),
 )
--- src/app/suman/(index)/(suman-index-core)/(suman-index-destination)/page.tsx
+++ src/app/suman/(index)/(suman-index-core)/(suman-index-destination)/page.tsx
@@ -1,22 +1,18 @@
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
-import type { ZodType } from 'zod'
 
-import { serverEnv } from '@/lib/env/server'
-import { runQuery } from '@/sanity/groqd-client'
+import { runDraftQuery, runQuery } from '@/sanity/groqd-client'
 
-import type { SumanIndexQuery} from '../(suman-index-server)/suman.index-query';
 import { sumanIndexQuery } from '../(suman-index-server)/suman.index-query'
 import SumanIndexBody from './suman.index-component'
 import { PreviewHomePage } from './suman.index-preview'
 
 const SumanSlugRoute = async () => {
   const { isEnabled: draftModeEnabled } = draftMode()
-  const token = serverEnv.SANITY_API_READ_TOKEN
-  const data = await runQuery<ZodType<SumanIndexQuery>>(
+  const fetchClient = draftModeEnabled ? runDraftQuery : runQuery
+  const data = await fetchClient(
     sumanIndexQuery,
     {},
-    draftModeEnabled ? token : undefined
   )
 
   if (!data) {
