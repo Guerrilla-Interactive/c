b9590a5d43430cdbb45deea4fcca7fe2fc8f32b2
suman chapai <sumanchapai@gmail.com>
2024-01-24 21:30:18 +0545

    Add utils for page metadata generation

--- src/app/chapai/(index)/(chapai-index-core)/(chapai-index-destination)/page.tsx
+++ src/app/chapai/(index)/(chapai-index-core)/(chapai-index-destination)/page.tsx
@@ -1,11 +1,19 @@
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
+import { generatePageMeta } from 'src/shame-utils/generate-page-meta-util'
 
 import { runDraftQuery, runQuery } from '@/sanity/groqd-client'
 
 import { chapaiIndexQuery } from '../(chapai-index-server)/chapai.index-query'
 import ChapaiIndexBody from './chapai.index-component'
 import { ChapaiPreview } from './chapai.index-preview'
+
+export const generateMetadata = async () => {
+  const { isEnabled: draftModeEnabled } = draftMode()
+  const fetchClient = draftModeEnabled ? runDraftQuery : runQuery
+  const data = await fetchClient(chapaiIndexQuery, {})
+  return generatePageMeta(data?.metadata)
+}
 
 const ChapaiIndexRoute = async () => {
   const { isEnabled: draftModeEnabled } = draftMode()
---
+++ src/shame-utils/generate-page-meta-util.ts
@@ -0,0 +1,44 @@
+import type { Metadata } from "next"
+import type { Image } from "sanity"
+
+import { urlForImage } from "@/sanity/sanity-utils/sanity-image"
+import type { MetadataProps } from "@/sanity/shame-queries/metadata.query"
+
+
+export const generatePageMeta = (
+	metadata: MetadataProps | undefined
+): Metadata => {
+	const title = metadata?.title || metadata?.seoSettings?.metadata?.title
+	const desc = metadata?.desc || metadata?.seoSettings?.metadata?.desc
+	const graphic = metadata?.graphic || metadata?.seoSettings?.metadata?.graphic
+
+	const imageUrl = graphic?.asset
+		? urlForImage(graphic as Image)
+			.width(1200)
+			.height(630)
+			.url()
+		: null
+
+	const metaTitle = title ?? undefined
+	const metaDesc = desc ?? undefined
+
+	return {
+		title: metaTitle,
+		description: metaDesc,
+		openGraph: {
+			title: metaTitle,
+			description: metaDesc,
+			images: imageUrl
+				? [
+					{
+						url: imageUrl,
+						width: 1200,
+						height: 630,
+						alt: graphic?.alt ?? "",
+					},
+				]
+				: undefined,
+		},
+	}
+}
+
