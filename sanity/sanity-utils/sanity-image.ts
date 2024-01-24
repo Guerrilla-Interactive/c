9e7d8023832e8270cb05cdf3ba82bb7aae77fb4e
suman chapai <sumanchapai@gmail.com>
2024-01-24 19:50:55 +0545

    Add sanity image url generator

---
+++ sanity/sanity-utils/sanity-image.ts
@@ -0,0 +1,13 @@
+import imageUrlBuilder from '@sanity/image-url'
+import type { Image, ImageUrlBuilder } from 'sanity'
+
+import { clientEnv } from '@/lib/env/client'
+
+const imageBuilder = imageUrlBuilder({
+  projectId: clientEnv.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: clientEnv.NEXT_PUBLIC_SANITY_DATASET,
+}) as ImageUrlBuilder
+
+export const urlForImage = (source: Image) => {
+  return imageBuilder?.image(source).auto('format').fit('max')
+}
