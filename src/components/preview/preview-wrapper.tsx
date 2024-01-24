23aac6afb7dd27466d95357ca11919a46b91f37d
suman chapai <sumanchapai@gmail.com>
2024-01-24 09:43:55 +0545

    Enhance preview wrapper component

--- src/components/preview/preview-wrapper.tsx
+++ src/components/preview/preview-wrapper.tsx
@@ -1,19 +1,24 @@
-"use client"
+'use client'
 
-import { useLiveQuery } from "next-sanity/preview"
+import { useLiveQuery } from 'next-sanity/preview'
 
 type DataComponentFn<T> = (data: T) => JSX.Element
 
 interface PreviewLoadingErrorHOCProps<T> {
-  initial: T,
-  query: string,
-  queryParams?: Record<string, string>,
+  initial: T
+  query: string
+  slug?: string
+  queryParams?: Record<string, string | Array<string>>
   successFn: DataComponentFn<T>
 }
-export function PreviewLoadingErrorHOC<T>(props: PreviewLoadingErrorHOCProps<T>) {
-  const [data, loading] = useLiveQuery<T>(props.initial, props.query)
+export function PreviewLoadingErrorHOC<T>(
+  props: PreviewLoadingErrorHOCProps<T>,
+) {
+  const [data, loading] = useLiveQuery<T>(props.initial, props.query, {
+    slug: props.slug,
+  })
   if (data) return props.successFn(data)
   if (loading) return <div>loading...</div>
-  console.error("data", data)
+  console.error('data', data)
   return <div>no data, check console for error details</div>
 }
