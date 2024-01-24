ccce8499fef4ac0df56c2e1671e9873b13ab2579
suman chapai <sumanchapai@gmail.com>
2024-01-24 15:12:17 +0545

    Migrate /chapai route to latest usecase

--- src/app/chapai/(index)/(chapai-index-core)/(chapai-index-destination)/page.tsx
+++ src/app/chapai/(index)/(chapai-index-core)/(chapai-index-destination)/page.tsx
@@ -1,23 +1,16 @@
 import { draftMode } from 'next/headers'
-import { notFound } from "next/navigation"
-import type { ZodType } from 'zod'
+import { notFound } from 'next/navigation'
 
-import { serverEnv } from '@/lib/env/server'
-import { runQuery } from '@/sanity/groqd-client'
+import { runDraftQuery, runQuery } from '@/sanity/groqd-client'
 
-import type { ChapaiIndexQuery} from '../(chapai-index-server)/chapai.index-query';
 import { chapaiIndexQuery } from '../(chapai-index-server)/chapai.index-query'
 import ChapaiIndexBody from './chapai.index-component'
-import { ChapaiPreview} from './chapai.index-preview'
+import { ChapaiPreview } from './chapai.index-preview'
 
 const ChapaiIndexRoute = async () => {
   const { isEnabled: draftModeEnabled } = draftMode()
-  const token = serverEnv.SANITY_API_READ_TOKEN
-  const data = await runQuery<ZodType<ChapaiIndexQuery>>(
-    chapaiIndexQuery,
-    {},
-    draftModeEnabled ? token : undefined
-  )
+  const fetchClient = draftModeEnabled ? runDraftQuery : runQuery
+  const data = await fetchClient(chapaiIndexQuery, {})
 
   if (!data) {
     return notFound()
